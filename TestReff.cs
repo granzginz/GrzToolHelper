536a365bf4beb828daf8eeb62d918c379d88a7a7
Ginanjar <ganjar56@gmail.com>
2021-04-14 14:30:47 +0700

    First Commit
    create basic helper tool to help program

---
+++ TestReff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GrzToolHelper
+{
+    public class TestReff
+    {
+        public TestReff()
+        {
+
+        } 
+        public string helloWorld(string text)
+        {
+            return text;
+        }
+
+    }
+}
